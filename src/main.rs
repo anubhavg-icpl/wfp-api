27822f4ee3d890f5e73598282d854b4bd4874d29
metkm <metinkorkmaz417@gmail.com>
2024-03-27 23:18:58 +0300

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use windows::{
-    core::{w, PWSTR},
+    core::{PCWSTR, PWSTR},
     Wdk::NetworkManagement::WindowsFilteringPlatform::{FwpmEngineOpen0, FwpmFilterAdd0},
     Win32::{
         Foundation::HANDLE,
@@ -7,7 +7,7 @@ use windows::{
             FwpmGetAppIdFromFileName0, FWPM_ACTION0, FWPM_CONDITION_ALE_APP_ID, FWPM_DISPLAY_DATA0,
             FWPM_FILTER0, FWPM_FILTER_CONDITION0, FWPM_LAYER_ALE_AUTH_CONNECT_V4, FWP_ACTION_BLOCK,
             FWP_BYTE_BLOB, FWP_BYTE_BLOB_TYPE, FWP_CONDITION_VALUE0, FWP_CONDITION_VALUE0_0,
-            FWP_MATCH_EQUAL,
+            FWP_EMPTY, FWP_MATCH_EQUAL, FWP_VALUE0,
         },
         System::Rpc::RPC_C_AUTHN_DEFAULT,
     },
@@ -23,15 +23,17 @@ fn main() {
         let app_id_ptr = &mut app_id as *mut FWP_BYTE_BLOB;
         let app_id_ptr_ptr = app_id_ptr as *mut *mut FWP_BYTE_BLOB;
 
-        FwpmGetAppIdFromFileName0(
-            w!(
-                r"C:\Program Files\WindowsApps\Microsoft.WindowsCalculator_11.2401.0.0_x64__8wekyb3d8bbwe\CalculatorApp.exe"
-            ),
-            app_id_ptr_ptr,
+        let path = String::from(
+            r#"C:\\Program Files\\WindowsApps\\Microsoft.WindowsCalculator_11.2401.0.0_x64__8wekyb3d8bbwe\\CalculatorApp.exe"#,
         );
+        let mut path_bits: Vec<u16> = path.encode_utf16().collect();
 
-        let mut name = String::from("Block layer");
-        let name_ptr = name.as_mut_ptr() as *mut u16;
+        FwpmGetAppIdFromFileName0(PCWSTR(path_bits.as_mut_ptr()), app_id_ptr_ptr);
+
+        let name = String::from("Block layer");
+        let mut name_bits: Vec<u16> = name.encode_utf16().collect();
+
+        println!("{:?}", *app_id_ptr);
 
         let mut condition = FWPM_FILTER_CONDITION0 {
             fieldKey: FWPM_CONDITION_ALE_APP_ID,
@@ -47,7 +49,7 @@ fn main() {
 
         let filter = FWPM_FILTER0 {
             displayData: FWPM_DISPLAY_DATA0 {
-                name: PWSTR(name_ptr),
+                name: PWSTR(name_bits.as_mut_ptr()),
                 ..Default::default()
             },
             layerKey: FWPM_LAYER_ALE_AUTH_CONNECT_V4,
@@ -55,7 +57,11 @@ fn main() {
                 r#type: FWP_ACTION_BLOCK,
                 ..Default::default()
             },
-            numFilterConditions: 0,
+            numFilterConditions: 1,
+            weight: FWP_VALUE0 {
+                r#type: FWP_EMPTY,
+                ..Default::default()
+            },
             filterCondition: &mut condition,
             ..Default::default()
         };
